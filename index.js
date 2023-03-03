ff3303160b55d32cd42036ad7141a5511955caf4
DwiFauzul07 <fauzultanjung05@gmail.com>
2023-03-03 21:08:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+    console.log("ada request masuk");
+    next();
+});
+
+//route
+app.get("/",(_req, res) => {
+    res.send("Fauzul");
+});
+
+app.listen(3000, () => {
+    console.log("Berhasil Jalan");
+});
